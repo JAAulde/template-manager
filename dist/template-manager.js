605d96f3b40e8be1ce6ada16fbc230a829419a8d
Jim Auldridge <auldridgej@gmail.com>
2016-08-14 19:31:07 -0400

    add built output

---
+++ dist/template-manager.js
@@ -0,0 +1,63 @@
+/* jslint */
+
+/**
+ * @file template-manager.js A simple manager for loading and caching templates
+ * @version 1.0.0
+ * @copyright Jim Auldridge <auldridgej@gmail.com> 2016
+ * @license MIT
+ * @see {@link https://github.com/JAAulde/template-manager|GitHub Repository}
+ */
+(function (context) {
+    'use strict';
+
+    var $ = context.jQuery,
+        template_manager,
+        cache = {},
+        fetchers = {
+            fetchCache: function (id) {
+                return cache[id] || null;
+            },
+            fetchElement: function (id) {
+                $('#' + id).each(function () {
+                    template_manager.cache(id, $.trim($(this).text()));
+
+                    return false;
+                });
+
+                return fetchers.fetchCache(id);
+            },
+            fetchNetwork: function (url) {
+                $.ajax({
+                    type: 'GET',
+                    url: url,
+                    dataType: 'html',
+                    async: false,
+                    success: function (data) {
+                        template_manager.cache(url, data);
+                    }
+                });
+
+                return fetchers.fetchCache(url);
+            }
+        };
+
+    context.template_manager = template_manager = {
+        get: function (id) {
+            var template = null,
+                methods = [
+                    'fetchNetwork',
+                    'fetchElement',
+                    'fetchCache'
+                ];
+
+            while (template === null && methods.length) {
+                template = fetchers[methods.pop()](id);
+            }
+
+            return template;
+        },
+        cache: function (id, template) {
+            cache[id] = template;
+        }
+    };
+}(this));
