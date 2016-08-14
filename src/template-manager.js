49e24289821ef16d86c0a73a285bcfa18967559a
Jim Auldridge <auldridgej@gmail.com>
2016-08-14 19:30:12 -0400

    add header comments and lint the code

--- src/template-manager.js
+++ src/template-manager.js
@@ -1,59 +1,63 @@
+/* jslint */
+
+/**
+ * @file template-manager.js A simple manager for loading and caching templates
+ * @version %%GULP_INJECT_VERSION%%
+ * @copyright Jim Auldridge <auldridgej@gmail.com> 2016
+ * @license MIT
+ * @see {@link https://github.com/JAAulde/template-manager|GitHub Repository}
+ */
 (function (context) {
-	'use strict';
+    'use strict';
 
-	var $ = context.jQuery,
-		template_manager,
-		cache = {},
-		fetchers = {
-			fetchCache: function (id) {
-				return cache[id] || null;
-			},
-			fetchElement: function (id) {
-				var template = null;
+    var $ = context.jQuery,
+        template_manager,
+        cache = {},
+        fetchers = {
+            fetchCache: function (id) {
+                return cache[id] || null;
+            },
+            fetchElement: function (id) {
+                $('#' + id).each(function () {
+                    template_manager.cache(id, $.trim($(this).text()));
 
-				$('#' + id).each(function () {
-					template_manager.cache(id, $.trim($(this).text()));
+                    return false;
+                });
 
-					return false;
-				});
+                return fetchers.fetchCache(id);
+            },
+            fetchNetwork: function (url) {
+                $.ajax({
+                    type: 'GET',
+                    url: url,
+                    dataType: 'html',
+                    async: false,
+                    success: function (data) {
+                        template_manager.cache(url, data);
+                    }
+                });
 
-				return fetchers.fetchCache(id);
-			},
-			fetchNetwork: function (url) {
-				var template = null;
+                return fetchers.fetchCache(url);
+            }
+        };
 
-				$.ajax({
-					type: 'GET',
-					url: url,
-					dataType: 'html',
-					async: false,
-					success: function (data) {
-						template_manager.cache(url, data);
-					}
-				});
+    context.template_manager = template_manager = {
+        get: function (id) {
+            var template = null,
+                methods = [
+                    'fetchNetwork',
+                    'fetchElement',
+                    'fetchCache'
+                ];
 
-				return fetchers.fetchCache(url);
-			}
-		};
+            while (template === null && methods.length) {
+                template = fetchers[methods.pop()](id);
+            }
 
-	context.template_manager = template_manager = {
-		get: function (id) {
-			var template = null,
-			methods = [
-				'fetchNetwork',
-				'fetchElement',
-				'fetchCache'
-			],
-			method;
-
-			while (template === null && methods.length) {
-				template = fetchers[methods.pop()](id);
-			}
-
-			return template;
-		},
-		cache: function (id, template) {
-			cache[id] = template;
-		}
-	};
+            return template;
+        },
+        cache: function (id, template) {
+            cache[id] = template;
+        }
+    };
 }(this));
